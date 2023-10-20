8a5f443d79ba71178aee39e20faa4ee2affd9e6e
Edmarques Lima <edmarquessantosdelima@gmail.com>
2023-10-19 21:37:53 -0300

    feat: ajust seed create rule

--- prisma/seeds/create_rules.ts
+++ prisma/seeds/create_rules.ts
@@ -3,19 +3,14 @@ import { prismaClient as prisma } from "../../src/external/clientDatabase/prisma
 async function main() {
   const locator = await prisma.rule.create({ data: { name: "locator" } });
 
-  console.log(">>>", locator);
+  console.log(locator);
 }
 
-(async () => {
-  try {
-    await main();
-
-    console.log("Deu booooooooooom!");
-  } catch (err) {
-    console.log("Errouuu", err);
-    process.exit(1);
-  } finally {
-    console.log("finalizouuuu");
-    await prisma.$disconnect();
-  }
-})();
+try {
+  await main();
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+} finally {
+  await prisma.$disconnect();
+}
